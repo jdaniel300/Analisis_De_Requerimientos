56cdccdb4f443e59c7b7eb8cf98178f6a7df69f6
MijaDs <106399497+MijaDs@users.noreply.github.com>
2025-04-17 00:56:37 -0600

    Vista asistencia
    incompleta

--- PlataformaAccionSocial/PlataformaAccionSocial/Controllers/HomeController.cs
+++ PlataformaAccionSocial/PlataformaAccionSocial/Controllers/HomeController.cs
@@ -27,7 +27,10 @@ namespace PlataformaAccionSocial.Controllers
         {
             return View();
         }
-
+        public IActionResult Asistencia()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
